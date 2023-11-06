187ff58d6112c275d0e0c11b4eb3042b04d0ede8
tmanso9 <tmanso92@gmail.com>
2023-11-06 15:55:13 +0000

    feat: callback function is async code

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
-const request = new XMLHttpRequest()
-
-request.addEventListener('readystatechange', () => {
-	// console.log(request, request.readyState)
-	if (request.readyState === 4) {
-		if (request.status === 200) {
-			console.log(request, request.responseText)
-		} else {
-			console.log('could not fetch the data')
+const getTodos = (callback) => {
+	const request = new XMLHttpRequest()
+	
+	request.addEventListener('readystatechange', () => {
+		// console.log(request, request.readyState)
+		if (request.readyState === 4) {
+			if (request.status === 200) {
+				callback(undefined, request.responseText)
+			} else {
+				callback('could not fetch data')
+			}
 		}
+	})
+	
+	request.open('GET', 'https://jsonplaceholder.typicode.com/todos/1')
+	request.send()
+}
+
+console.log(1)
+console.log(2)
+
+
+getTodos((err, data) => {
+	if (err) {
+		console.log(err)
+	} else {
+		console.log(data)
 	}
 })
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos')
-request.send()
-
-
+console.log(3)
+console.log(4)
