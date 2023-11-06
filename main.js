f9fe5c93de61e624f58d815a22f9d28d15ffbdb0
tmanso9 <tmanso92@gmail.com>
2023-11-06 16:06:12 +0000

    feat: parsing JSON

--- main.js
+++ main.js
@@ -5,20 +5,20 @@ const getTodos = (callback) => {
 		// console.log(request, request.readyState)
 		if (request.readyState === 4) {
 			if (request.status === 200) {
-				callback(undefined, request.responseText)
+				const data = JSON.parse(request.responseText)
+				callback(undefined, data)
 			} else {
 				callback('could not fetch data')
 			}
 		}
 	})
 	
-	request.open('GET', 'https://jsonplaceholder.typicode.com/todos/1')
+	request.open('GET', 'todos.json')
 	request.send()
 }
 
 console.log(1)
 console.log(2)
-
 
 getTodos((err, data) => {
 	if (err) {
