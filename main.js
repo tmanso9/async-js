cd1892f09b5b888fd40e83f83fff3c18c9a8e924
tmanso9 <tmanso92@gmail.com>
2023-11-06 16:38:59 +0000

    feat: returning promise from getTodos

--- main.js
+++ main.js
@@ -1,30 +1,24 @@
-const getTodos = (path, callback) => {
-	const request = new XMLHttpRequest()
-
-	request.addEventListener('readystatechange', () => {
-		// console.log(request, request.readyState)
-		if (request.readyState === 4) {
-			if (request.status === 200) {
-				const data = JSON.parse(request.responseText)
-				callback(undefined, data)
-			} else {
-				callback('could not fetch data')
-			}
-		}
-	})
-
-	request.open('GET', path)
-	request.send()
-}
-
-const getSomething = () => {
+const getTodos = (path) => {
 	return new Promise((resolve, reject) => {
-		resolve('some data')
-		// reject('some error')
+		const request = new XMLHttpRequest()
+
+		request.addEventListener('readystatechange', () => {
+			if (request.readyState === 4) {
+				if (request.status === 200) {
+					const data = JSON.parse(request.responseText)
+					resolve(data)
+				} else {
+					reject('could not fetch data')
+				}
+			}
+		})
+
+		request.open('GET', path)
+		request.send()
 	})
 }
 
-getSomething()
+getTodos('todos/mario.json')
 	.then((data) => {
 		console.log(data)
 	})
