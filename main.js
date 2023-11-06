4028e009515395618fa41ab176b11185652569a9
tmanso9 <tmanso92@gmail.com>
2023-11-06 16:58:22 +0000

    feat: async and await

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
-const getTodos = (path) => {
-	fetch(path)
-		.then((response) => {
-			if (!response.ok) {
-				throw new Error('error fetching')
-			}
-			console.log(response)
-			return response.json()
-		})
-		.then((data) => {
-			console.log(data)
-		})
-		.catch((err) => {
-			console.error(err)
-		})
+const getTodos = async (path) => {
+	const response = await fetch(path)
+	try {
+		if (!response.ok) {
+			throw new Error('error fetching')
+		}
+		const data = await response.json()
+		return data
+	} catch (err) {
+		throw new Error(err)
+	}
 }
 
 getTodos('todos/luigi.json')
+	.then((data) => console.log(data))
+	.catch((err) => console.error(err))
